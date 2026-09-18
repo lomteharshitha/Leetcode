d2244e043674ae545acf458430e8c8a4d0341272
lomteharshitha <167612529+lomteharshitha@users.noreply.github.com>
2026-09-18 20:16:19 +0530

    Time: 11 ms (12.76%), Space: 61.9 MB (5.41%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let map=new Map()
+    for(let val of nums){
+        map.set(val,(map.get(val)||0)+1)
+    }
+    for(let [key,val] of map){
+            if(val>(nums.length/2)){
+                return key
+            }
+    }
+    return false
+};
